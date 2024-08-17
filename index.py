3aba3ffeaefe2d5465d0fda0b8db55271e45946c
lemis <lemisogideon@gmail.com>
2024-08-17 15:30:55 +0300

    first commit

---
+++ index.py
@@ -0,0 +1,63 @@
+# #favourite number
+# favouriteNumber = 55
+# print(f'My Favourite number is {favouriteNumber}')
+
+# #String
+# greeting = 'Hello Plp Academy'
+# print(greeting)
+
+# #Boolean
+# is_python_fun = True
+# is_python_boring = False
+# print(f"Is python fun? {is_python_fun}")
+# print(f"Is python fun? {is_python_boring}")
+
+#Control flow
+# speed = 80
+# if speed > 70:
+#   print("Thats Fast")
+#   if speed <= 40:
+#     print("Thats safe")
+#   else:
+#     print("Thats Slow")
+
+#Loops
+# for i in range(8):
+#   print(f"I Love PLP Academy {i}")
+
+# countDown = 10
+# while countDown > 0:
+#   print(f"The count is {countDown}")
+#   countDown = 1
+# print("The Count down is successful!!!")
+
+#Functions
+# def greet(person):
+#   return f"Hello How are you {person}"
+# #Apply the function
+# print(greet("Gideon"))
+# print(greet ("Jane"))
+
+#Lists and Dictionaries
+# fruits_list = ["Apple", "Mango", "Melon", "Avocado"]
+# fruits_tuple = ("Banana", "Pineapple", "Orange")
+# print(fruits_list)
+# print(fruits_tuple)
+
+#Dictionary
+# Contacts = {
+#   "Alice": "123456",
+#   "John": "987654",
+#   "Mark": "192837"
+# }
+# print(f"Mark contact's is {Contacts["Mark"]}")
+
+#Modules
+import math
+#Use function in the module math
+result = math.sqrt(49)
+print(f"The Square root of 49 is {result}")
+
+
+
+
